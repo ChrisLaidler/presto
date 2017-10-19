869b4e5649df07c3680ab44946c20044e1b5d291
Chris Laidler <chris.laidler@gmail.com>
2017-10-19 15:41:41 +0200

    Add a nicer templated complex number struct

--- include/cuda_accel.h
+++ include/cuda_accel.h
@@ -588,19 +588,86 @@ typedef struct resThrds resThrds;
 ///< A complex float in device texture memory
 typedef cudaTextureObject_t fCplxTex;
 
-///< A complex number data type
-typedef struct fcomplexcu
-{
-    float		r;		///< Real Component
-    float		i;		///< Imaginary Component
-} fcomplexcu;
+typedef long double quad;
 
-///< A complex number data type
-typedef struct dcomplexcu
+///< A templated complex number data type
+template<typename T> struct complex_t
 {
-    double		r;		///< Real Component
-    double		i;		///< Imaginary Component
-} dcomplexcu;
+    T		r;		///< Real Component
+    T		i;		///< Imaginary Component
+
+    // default + parameterised constructor
+    __host__ __device__ complex_t(T r=0, T i=0)
+    : r(r), i(i)
+    {
+    }
+
+    __host__ __device__ complex_t& operator =(const double v)
+    {
+      r = v;
+      i = v;
+      return *this;
+    }
+
+    __host__ __device__ complex_t& operator+=(const complex_t& rhs) { r += rhs.r; i += rhs.i; return *this; }
+    __host__ __device__ complex_t& operator-=(const complex_t& rhs) { r -= rhs.r; i -= rhs.i; return *this; }
+
+    __host__ __device__ complex_t operator+(const complex_t& a) const
+    {
+      return complex_t(a.r+r, a.i+i);
+    }
+
+    __host__ __device__ complex_t operator-(const complex_t& a) const
+    {
+      return complex_t(r-a.r, i-a.i);
+    }
+
+    __host__ __device__ complex_t operator*(const complex_t& a) const
+    {
+      return complex_t(r*a.r-i*a.i, r*a.i+i*a.r);
+    }
+
+    __host__ __device__ complex_t& operator*=(const complex_t& a)
+    {
+      T tmpr = r;
+      r = r*a.r-i*a.i;
+      i = tmpr*a.i+i*a.r;
+      return *this;
+    }
+
+    __host__ __device__ complex_t operator/(const double v) const
+    {
+      return complex_t(r/v, i/v);
+    }
+
+    __host__ __device__ complex_t operator*(const double v) const
+    {
+      return complex_t(r*v, i*v);
+    }
+};
+
+template<typename T> complex_t<T> operator-(const complex_t<T> &f) {return complex_t<T>(-f.r, -f.i);}
+
+template<typename T> complex_t<T> operator*(const float &v, const complex_t<T> &f) {return complex_t<T>(v*f.r, v*f.i);}
+template<typename T> complex_t<T> operator/(const float &v, const complex_t<T> &f) {return complex_t<T>(v*f.r/(f.r*f.r+f.i*f.i), -v*f.i/(f.r*f.r+f.i*f.i) );}
+template<typename T> complex_t<T> operator-(const float &v, const complex_t<T> &f) {return complex_t<T>(v-f.r, -f.i);}
+template<typename T> complex_t<T> operator+(const float &v, const complex_t<T> &f) {return complex_t<T>(v+f.r, f.i);}
+
+template<typename T> complex_t<T> operator*(const double &v, const complex_t<T> &f) {return complex_t<T>(v*f.r, v*f.i);}
+template<typename T> complex_t<T> operator/(const double &v, const complex_t<T> &f) {return complex_t<T>(v*f.r/(f.r*f.r+f.i*f.i), -v*f.i/(f.r*f.r+f.i*f.i) );}
+template<typename T> complex_t<T> operator-(const double &v, const complex_t<T> &f) {return complex_t<T>(v-f.r, -f.i);}
+template<typename T> complex_t<T> operator+(const double &v, const complex_t<T> &f) {return complex_t<T>(v+f.r, f.i);}
+
+template<typename T> complex_t<T> operator*(const quad &v, const complex_t<T> &f) {return complex_t<T>(v*f.r, v*f.i);}
+template<typename T> complex_t<T> operator/(const quad &v, const complex_t<T> &f) {return complex_t<T>(v*f.r/(f.r*f.r+f.i*f.i), -v*f.i/(f.r*f.r+f.i*f.i) );}
+template<typename T> complex_t<T> operator-(const quad &v, const complex_t<T> &f) {return complex_t<T>(v-f.r, -f.i);}
+template<typename T> complex_t<T> operator+(const quad &v, const complex_t<T> &f) {return complex_t<T>(v+f.r, f.i);}
+
+
+typedef complex_t<float>  fcomplexcu;
+typedef complex_t<double> dcomplexcu;
+typedef complex_t<quad>   qcomplexcu;
+
 
 ///< Basic accel search candidate to be used in CUDA kernels
 ///< Note this may not be the best choice on a GPU as it has a bad size
