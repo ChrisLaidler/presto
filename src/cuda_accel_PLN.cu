58aa57b08f0aecf354b06bb03c83277e6ed86e71
Chris Laidler <chris.laidler@gmail.com>
2017-10-28 12:41:17 +0200

    SHF Plots

--- src/cuda_accel_PLN.cu
+++ src/cuda_accel_PLN.cu
@@ -2251,6 +2251,12 @@ ACC_ERR_CODE ffdotPln_calcCols( cuRzHarmPlane* pln, int64_t flags, int colDiviso
 
       FOLD // Select a good power of two number of columns  .
       {
+	if ( target_noCol <= 32 )	// DBG remove this!
+	{
+	  pln->blkCnt	= MIN(MAX_OPT_SFL_NO,exp2(round(log2(target_noCol))));	// Closes power of tow greater than the plane size
+	}
+	else
+	{
 	if ( pln->rSize > 8 )	// TODO: Determine this bound
 	{
 	  double diff = 1.0;
@@ -2276,6 +2282,7 @@ ACC_ERR_CODE ffdotPln_calcCols( cuRzHarmPlane* pln, int64_t flags, int colDiviso
 	{
 	  // Smaller than one so pretty much have to have a width of 1
 	  pln->blkCnt	= 1;
+	}
 	}
       }
 
