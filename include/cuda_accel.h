94681611aa47c0e65ead78132ba221c99234fd69
Chris Laidler <chris.laidler@gmail.com>
2017-11-17 15:14:39 +0200

    Enable SAS counting

--- include/cuda_accel.h
+++ include/cuda_accel.h
@@ -142,7 +142,7 @@ extern "C"
 //#define 		WITH_SAS_CPU			///< Compile with CPU Sum and search - (deprecated) - This is way to slow!
 
 ////////	Candidate
-//#define  		WITH_SAS_COUNT			///< Allow counting of candidates in sum & search kernel
+#define  		WITH_SAS_COUNT			///< Allow counting of candidates in sum & search kernel
 
 
 ////////	Optimisation
